a82b54417df9ce1fd00989d69da7f35d3beb1b20
Peter COLLE <peter.colle@ymail.com>
2018-08-22 09:21:23 +0200

    translate in english

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 i = 0 ;
      imax = document.images.length - 1 ;
-     setTimeout(suivante, 2000) ;
+     setTimeout(next, 2000) ;
 
-     function suivante()
+     function next()
      {
      document.images[i].style.display = "none" ;
      if ( i < imax )
@@ -10,5 +10,5 @@ i = 0 ;
      else
           i=0;
      document.images[i].style.display = "block";
-     setTimeout(suivante, 2000) ;
+     setTimeout(next, 2000) ;
      }
